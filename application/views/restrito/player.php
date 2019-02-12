df0096fd9e8694cc342b6981a0158e721e50f0d4
Pedro Galocha <pedrogalocha@gmail.com>
2019-02-12 18:26:26 -0200

    Ta dificil
    Tentando listar conquistas.

--- application/views/restrito/player.php
+++ application/views/restrito/player.php
@@ -27,7 +27,7 @@
     <div class="col-sm-9">
       <br>
       <h1 class="h1-player">Conquistas</h1>
-      <?php while ($conquistas = mysql_fetch_assoc($conquistas) ) { ?>
+      <?php foreach ($conquistas as $conq){ ?>
     	<div class="col-sm-3">       
           <div class="tab-content">
             <div class="tab-pane active" id="home">
@@ -37,7 +37,6 @@
                         <div class="card" style="width: 18rem;">
                           <div class="card-body">
                             <h5 class="card-title"><?php print_r($conquistas['nome_con']); ?></h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" class="card-link">Card link</a>
                             <a href="#" class="card-link">Another link</a>
