c4be8fc94a27f7981a6f574d836223d10eecd244
Pedro Galocha <pedrogalocha@gmail.com>
2019-02-19 16:37:17 -0300

    Alterações classe 1.1
    - Tentando modelar alguma ideia para sistema de level up.

--- application/models/Classes_Model.php
+++ application/models/Classes_Model.php
@@ -10,6 +10,14 @@ class Classes_Model extends CI_Model
 
     
     public function pegar_classes(){
-      $sql = "";
+      $sql = "SELECT c.id, c.class_name,h.nome nome_habilidade, h.descricao desc_habilidade FROM classe_habilidade ch
+      Inner Join class c on ch.id_classe = c.id
+      Inner Join habilidades h on ch.id_habilidade = h.id ;";
+      $heroe = $this->db->query($sql);
+      if($resultado!=null){
+        return $heroe->result();
+      } else {
+        return null;
+      }
     }
 }
