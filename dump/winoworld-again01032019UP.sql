acabac34bbc505f31c78dc31fb74498fa0b57469
Pedro Galocha <pedrogalocha@gmail.com>
2019-03-01 18:05:37 -0300

    BACKUP BANCO 1.1
    - Feito Backup do banco com alterações
    - Alterado url_base do codeigniter.

--- dump/winoworld-again01032019UP.sql
+++ dump/winoworld-again01032019UP.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `winoworld` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `winoworld`;
 -- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
 -- Host: localhost    Database: winoworld
@@ -390,4 +388,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-01 18:05:03
+-- Dump completed on 2019-03-01 18:04:52
