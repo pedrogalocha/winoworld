bab9ab77429e761320a1f484234eb108bb37d3c3
Pedro Galocha <pedrogalocha@gmail.com>
2019-03-13 11:59:58 -0300

    Backup Banco 1.1
    - Somente segurança.

---
+++ dump/winoworld-again13032019.sql
@@ -0,0 +1,392 @@
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
+--
+-- Host: localhost    Database: winoworld
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `class`
+--
+
+DROP TABLE IF EXISTS `class`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `class` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `class_name` varchar(45) DEFAULT NULL,
+  `descricao` varchar(200) DEFAULT NULL,
+  `avatar` varchar(45) DEFAULT NULL,
+  `id_habilidade` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `class`
+--
+
+LOCK TABLES `class` WRITE;
+/*!40000 ALTER TABLE `class` DISABLE KEYS */;
+INSERT INTO `class` VALUES (1,'Noob','Fraco, um ninguém','sob_1.jpg',NULL);
+/*!40000 ALTER TABLE `class` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `conquistas`
+--
+
+DROP TABLE IF EXISTS `conquistas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `conquistas` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nome_con` varchar(45) DEFAULT NULL,
+  `xp` int(11) DEFAULT NULL,
+  `descricao` varchar(45) DEFAULT NULL,
+  `imagem` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `conquistas`
+--
+
+LOCK TABLES `conquistas` WRITE;
+/*!40000 ALTER TABLE `conquistas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `conquistas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `conquistas_feitas`
+--
+
+DROP TABLE IF EXISTS `conquistas_feitas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `conquistas_feitas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `conquista_id` int(11) DEFAULT NULL,
+  `data` date DEFAULT NULL,
+  `jogador_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `conquistas_feitas`
+--
+
+LOCK TABLES `conquistas_feitas` WRITE;
+/*!40000 ALTER TABLE `conquistas_feitas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `conquistas_feitas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `habilidades`
+--
+
+DROP TABLE IF EXISTS `habilidades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `habilidades` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `desc` varchar(45) DEFAULT NULL,
+  `efeito` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `habilidades`
+--
+
+LOCK TABLES `habilidades` WRITE;
+/*!40000 ALTER TABLE `habilidades` DISABLE KEYS */;
+/*!40000 ALTER TABLE `habilidades` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `habilidades_kit`
+--
+
+DROP TABLE IF EXISTS `habilidades_kit`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `habilidades_kit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hab1` int(11) DEFAULT NULL,
+  `hab2` int(11) DEFAULT NULL,
+  `hab3` int(11) DEFAULT NULL,
+  `hab4` int(11) DEFAULT NULL,
+  `hab5` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `habilidades_kit`
+--
+
+LOCK TABLES `habilidades_kit` WRITE;
+/*!40000 ALTER TABLE `habilidades_kit` DISABLE KEYS */;
+/*!40000 ALTER TABLE `habilidades_kit` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `level`
+--
+
+DROP TABLE IF EXISTS `level`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `level` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `level` int(11) DEFAULT NULL,
+  `requisitos` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `level`
+--
+
+LOCK TABLES `level` WRITE;
+/*!40000 ALTER TABLE `level` DISABLE KEYS */;
+INSERT INTO `level` VALUES (1,0,NULL);
+/*!40000 ALTER TABLE `level` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `log_jogadores`
+--
+
+DROP TABLE IF EXISTS `log_jogadores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `log_jogadores` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nome_jogador` varchar(45) DEFAULT NULL,
+  `xp` int(11) DEFAULT NULL,
+  `level_id` int(11) DEFAULT NULL,
+  `class_id` int(11) DEFAULT NULL,
+  `data` datetime DEFAULT NULL,
+  `tma` time DEFAULT NULL,
+  `zumbis_mortos` int(11) DEFAULT NULL,
+  `sem_categoria` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `log_jogadores`
+--
+
+LOCK TABLES `log_jogadores` WRITE;
+/*!40000 ALTER TABLE `log_jogadores` DISABLE KEYS */;
+INSERT INTO `log_jogadores` VALUES (1,'João Uchiashi',0,0,0,'2019-03-01 16:00:30','00:00:00',0,682),(2,'João Uchiashi',0,0,0,'2019-03-01 16:01:57','00:00:00',0,682),(3,'João Uchiashi',0,0,0,'2019-03-01 16:07:57','00:00:00',0,682),(4,'João Uchiashi',0,0,0,'2019-03-01 16:10:59','00:00:00',0,682),(5,'João Uchiashi',0,0,0,'2019-03-01 16:14:01','00:00:00',0,682),(6,'Douglas Dias',512,0,0,'2019-03-12 08:25:16','00:25:31',116,0);
+/*!40000 ALTER TABLE `log_jogadores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mes_atual`
+--
+
+DROP TABLE IF EXISTS `mes_atual`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `mes_atual` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `inicio_mes` datetime DEFAULT NULL,
+  `fim_mes` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mes_atual`
+--
+
+LOCK TABLES `mes_atual` WRITE;
+/*!40000 ALTER TABLE `mes_atual` DISABLE KEYS */;
+INSERT INTO `mes_atual` VALUES (1,'2019-03-01 00:00:00','2019-03-31 23:59:59');
+/*!40000 ALTER TABLE `mes_atual` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `players`
+--
+
+DROP TABLE IF EXISTS `players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `players` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `class_id` varchar(45) DEFAULT NULL,
+  `hp` int(11) DEFAULT NULL,
+  `hp_total` int(11) DEFAULT NULL,
+  `avatar` varchar(45) DEFAULT NULL,
+  `xp` int(11) DEFAULT NULL,
+  `liberado` int(11) DEFAULT NULL,
+  `chance` int(11) DEFAULT NULL,
+  `users_id` int(11) DEFAULT NULL,
+  `level_id` int(11) DEFAULT NULL,
+  `glpi_id` int(11) DEFAULT NULL,
+  `zumbis_mortos` int(11) DEFAULT NULL,
+  `tma` time DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `players`
+--
+
+LOCK TABLES `players` WRITE;
+/*!40000 ALTER TABLE `players` DISABLE KEYS */;
+INSERT INTO `players` VALUES (1,'João Uchiashi','1',26,50,'soldier.png',592,0,0,1,1,968,92,'00:22:36'),(2,'Giovani Di Nolla','1',50,50,'sob_2.png',606,0,0,2,1,945,106,'00:36:17'),(3,'Paulo Aquino','1',50,50,'sob_5.png',1097,0,0,3,1,972,47,'00:31:22'),(4,'Fabio Santos','1',50,50,'sob_1.png',904,0,0,4,1,63,4,'00:15:15'),(5,'Douglas Dias','1',50,50,'sob_4.png',19,0,1,5,1,952,19,'00:25:19');
+/*!40000 ALTER TABLE `players` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `task` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name_task` varchar(100) DEFAULT NULL,
+  `xp` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task`
+--
+
+LOCK TABLES `task` WRITE;
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+INSERT INTO `task` VALUES (8,'Manutenção de equipamento',50),(9,'Pensar e criar formas melhores de derrotar os zumbis',100),(10,'Atualizar registro de missões e sobre as criaturas',50),(11,'Movimentar os equipamentos entre lugares.',25),(12,'Alterar os canais de comunicação da equipe',1),(13,'Modificações na torre de rádio.',100),(14,'Patrulhas e alertas sobre possíveis ataques.',50),(15,'Atualizar registros sobre o que temos em estoque.',50),(16,'Rotinas dentro da base.',250),(17,'Reconhecimento de campo proximo a base.',25);
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task_feita`
+--
+
+DROP TABLE IF EXISTS `task_feita`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `task_feita` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `task_id` int(11) DEFAULT NULL,
+  `data_conclusao` datetime DEFAULT NULL,
+  `player_id` int(11) DEFAULT NULL,
+  `xp_total` int(11) DEFAULT NULL,
+  `quantidade` int(11) DEFAULT NULL,
+  `n_chamado` int(11) DEFAULT NULL,
+  `id_player_log` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task_feita`
+--
+
+LOCK TABLES `task_feita` WRITE;
+/*!40000 ALTER TABLE `task_feita` DISABLE KEYS */;
+INSERT INTO `task_feita` VALUES (1,9,'2019-03-06 16:20:02',4,400,4,0,NULL),(2,9,'2019-03-06 16:20:19',1,400,4,0,NULL),(3,9,'2019-03-06 16:20:39',2,200,2,0,NULL),(4,9,'2019-03-06 16:21:23',3,200,2,0,NULL),(5,9,'2019-03-06 16:21:50',3,100,1,0,NULL),(6,8,'2019-03-07 08:28:16',0,100,2,19084,5),(7,8,'2019-03-07 09:56:58',2,300,6,0,NULL),(8,8,'2019-03-07 09:57:15',3,50,1,0,NULL),(9,8,'2019-03-07 09:57:32',0,300,6,0,5),(10,13,'2019-03-07 11:59:38',1,100,1,0,NULL),(11,9,'2019-03-11 17:00:50',3,600,6,0,NULL),(12,13,'2019-03-13 11:11:34',4,400,4,0,NULL),(13,9,'2019-03-13 11:12:21',4,100,1,0,NULL),(14,9,'2019-03-13 11:12:24',3,100,1,0,NULL);
+/*!40000 ALTER TABLE `task_feita` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `adm` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'joao.uchiashi','123','Jogador'),(2,'giovani.souza','123','Jogador'),(3,'paulo.aquino','123','Jogador'),(4,'fabio.santos','123','Jogador'),(5,'douglas.dias','123','Jogador');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `zumbis_aux`
+--
+
+DROP TABLE IF EXISTS `zumbis_aux`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `zumbis_aux` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `log_zumbis` int(11) DEFAULT NULL,
+  `id_player` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `zumbis_aux`
+--
+
+LOCK TABLES `zumbis_aux` WRITE;
+/*!40000 ALTER TABLE `zumbis_aux` DISABLE KEYS */;
+INSERT INTO `zumbis_aux` VALUES (1,4,1),(2,0,2),(3,0,3),(4,0,4),(5,0,5);
+/*!40000 ALTER TABLE `zumbis_aux` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-03-13 11:59:13
